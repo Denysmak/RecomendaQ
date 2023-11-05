7be3f2c82cf63a69981282fdb784d70c590cd596
LedroDs <leandro87dev@gmail.com>
2023-11-04 22:37:35 -0300

    "acrescentando mais plotagens"

--- pages/3_Boxplots.py
+++ pages/3_Boxplots.py
@@ -8,12 +8,12 @@ import seaborn as sns
 # Configuração do estilo do gráfico usando Seaborn
 sns.set_style('darkgrid')
 
-
 dataset = 'data/Marvel_Comics.parquet'
 df = pd.read_parquet(dataset)
 
 # Substitua os valores "Free" por "0.00" na coluna 'Price'
 df['Price'] = df['Price'].str.replace('Free', '0.00')
+
 # Em seguida, converta a coluna 'Price' em tipo numérico
 df['Price'] = df['Price'].str.replace('$', '').astype(float)
 
@@ -22,6 +22,15 @@ df['start_year'] = df['active_years'].str.extract(r'(\d{4})').astype(float)
 # Preenche com NaN se houver valores inválidos
 df['start_year'].fillna(np.nan, inplace=True)
 
+# Tratando as inconsistências na coluna de Rating
+# Normalizando todas as classificações para letras minúsculas
+df['Rating'] = df['Rating'].str.lower()
+# Agrupando os valores "no rating" e nulos como "não classificados"
+df['Rating'].replace(['no rating', None], 'não classificados', inplace=True)
+
+# Filtrando as 8 primeiras classificações de idade (ratings)
+top_ratings = df['Rating'].value_counts().index[:8]
+df_filtered = df[df['Rating'].isin(top_ratings)]
 
 num_columns = df.select_dtypes(exclude='object').columns
 cat_columns = df.select_dtypes(include='object').columns
@@ -48,7 +57,6 @@ start_years = counts_by_interval.index
 end_years = start_years + 9
 interval_labels = [f"{start}-{end}" for start, end in zip(start_years, end_years)]
 plt.xticks(range(len(start_years)), interval_labels)
-
 st.pyplot(plt)
 
 # Visualização: Distribuição das Classificações de Idade (Rating)
@@ -58,4 +66,12 @@ sns.countplot(x='Rating', data=df, order=df['Rating'].value_counts().index[:8], 
 plt.title('Distribuição das 8 primeiras Classificações de Idade (Rating)')
 plt.xlabel('Classificação de Idade')
 plt.ylabel('Contagem')
+st.pyplot(plt)
+
+# Criando um gráfico de dispersão com Matplotlib
+plt.figure(figsize=(10, 6))
+plt.scatter(df_filtered['Rating'], df_filtered['Price'], alpha=0.5, color='blue')
+plt.title('Relação entre Preço e Classificação de Idade')
+plt.xlabel('Classificação de Idade (Rating)')
+plt.ylabel('Preço')
 st.pyplot(plt)

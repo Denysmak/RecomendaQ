da10cfb93b315f0e146258f6cf0e776dbf2cda6b
LedroDs <leandro87dev@gmail.com>
2023-11-05 02:03:09 -0300

    envia novo plot + correções

--- pages/3_Boxplots.py
+++ pages/3_Boxplots.py
@@ -19,12 +19,16 @@ df['start_year'] = df['active_years'].str.extract(r'(\d{4})').astype(float)
 df['start_year'].fillna(np.nan, inplace=True)
 
 # Tratando as inconsistências na coluna de Rating
-df['Rating'] = df['Rating'].str.lower()
+df['Rating'] = df['Rating'].str.lower().str.replace('rated', '').str.strip()
 df['Rating'].replace(['no rating', None], 'não classificados', inplace=True)
 
 # Filtrando as 8 primeiras classificações de idade (ratings)
 top_ratings = df['Rating'].value_counts().index[:8]
 df_filtered = df[df['Rating'].isin(top_ratings)]
+df_filtered_with_unrated = df[df['Rating'].isin(top_ratings)]
+
+# Excluindo as não classificadas na versão sem não classificadas
+df_filtered_without_unrated = df[df['Rating'].isin(top_ratings) & (df['Rating'] != 'não classificados')]
 
 num_columns = df.select_dtypes(exclude='object').columns
 cat_columns = df.select_dtypes(include='object').columns
@@ -45,9 +49,9 @@ st.pyplot(plt)
 st.write('\n')
 
 # Visualização: Distribuição das Classificações de Idade (Rating)
-st.subheader('Distribuição das 8 primeiras Classificações de Idade (Rating)')
+st.subheader('Distribuição das principais Classificações indicativas (Rating)')
 plt.figure(figsize=(10, 6))
-sns.countplot(x='Rating', data=df, order=df['Rating'].value_counts().index[:8], color='blue')
+sns.countplot(x='Rating', data=df_filtered_with_unrated, order=top_ratings, color='blue')
 plt.title('Distribuição das 8 primeiras Classificações de Idade (Rating)')
 plt.xlabel('Classificação de Idade')
 plt.ylabel('Contagem')
@@ -55,8 +59,19 @@ st.pyplot(plt)
 
 st.write('\n')
 
+# Versão 2: Excluindo as Não Classificadas
+st.subheader('Distribuição das principais Classificações indicativas (Excluindo Não Classificadas)')
+plt.figure(figsize=(10, 6))
+sns.countplot(x='Rating', data=df_filtered_without_unrated, order=top_ratings, color='blue')
+plt.title('Distribuição das 8 primeiras Classificações de Idade (Excluindo Não Classificadas)')
+plt.xlabel('Classificação de Idade')
+plt.ylabel('Contagem')
+st.pyplot(plt)
+
+st.write('\n')
+
 # Criando um gráfico de dispersão
-st.subheader('Gráfico de Dispersão: Preço vs. Classificação de Idade')
+st.subheader('Gráfico de Dispersão: Preço vs Classificação de Idade')
 plt.figure(figsize=(10, 6))
 plt.scatter(df_filtered['Rating'], df_filtered['Price'], alpha=0.5, color='blue')
 plt.title('Relação entre Preço e Classificação de Idade')
